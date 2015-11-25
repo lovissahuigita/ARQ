d884f9b2b02b8ffe70ffcdae84da7406b4d47eb0
lovissahuigita <lovissahuigita@gatech.edu>
2015-11-24 22:45:54 -0500

    documented recv buffer + debug message

--- RxP/RecvBuffer.py
+++ RxP/RecvBuffer.py
@@ -9,16 +9,29 @@ class RecvBuffer:
     __logger = Util.setup_logger()
 
     def __init__(self, buffer_size=int(1024)):
+        """ Creates a new receive buffer
+        :param buffer_size: the size of the buffer
+        :return: None
+        """
         class_lock = threading.Lock()
         self.__empty_cond = threading.Condition(class_lock)
         self.__resize_cond = threading.Condition(class_lock)
         self.__recv_buffer = collections.deque(maxlen=buffer_size)
+        self.__logger.info("Receive Buffer has been created. Size: %d" % self.__recv_buffer.maxlen)
 
     # return buffer capacity in segment
     def get_buffer_size(self):
+        """ Get the size of the buffer
+        :return: The size of the buffer
+        """
+        self.__logger.info("Get Buffer Size: %d" % min(self.__recv_buffer.maxlen, 2147483647))
         return min(self.__recv_buffer.maxlen, 2147483647)
 
     def set_buffer_size(self, size_in_segment):
+        """ Sets the size of the buffer
+        :param size_in_segment: the desired size of the buffer
+        :return: None
+        """
         self.__resize_cond.acquire()
         self.__resize_cond.wait_for(len(self.__recv_buffer) < size_in_segment)
         # once the recv buffer contains less segment than requested new size:
@@ -27,27 +40,38 @@ class RecvBuffer:
             maxlen=size_in_segment
         )
         self.__resize_cond.release()
+        self.__logger.info("Buffer Size is: %d" % self.__recv_buffer.maxlen)
 
-    # return current window size in segment
     def get_window_size(self):
+        """ Returns current window size in segment
+        :return: the size of the window
+        """
+        self.__logger.info("Window Size: %d" % self.__recv_buffer.maxlen - len(self.__recv_buffer))
         return self.__recv_buffer.maxlen - len(self.__recv_buffer)
 
     def put(self, ack_num, inbound_segment):
+        """ Puts a segment into the receive buffer
+        :param ack_num: the acknowledgement number of the segment
+        :param inbound_segment: the segment to be put
+        :return: the acknowledgement number
+        """
         buffer = self.__recv_buffer
+        self.__logger.info("Buffer Size: %d" % len(buffer))
         self.__empty_cond.acquire()
         if len(buffer) < buffer.maxlen and inbound_segment.get_seq_num() == \
                 ack_num:
             buffer.append(inbound_segment)
+            self.__logger.info("Buffer Size: %d" % len(buffer))
             ack_num += max(len(inbound_segment.get_data()), 1)
         self.__empty_cond.notify()
         self.__empty_cond.release()
         return ack_num
 
-    # Take buffered segment's data.
-    #
-    # @max_read          max number of bytes to be read
-    # return data   list of data bytes with at most @max_read long
     def take(self, max_read):
+        """ Takes buffered segment's data.
+        :param max_read: the maximum byte stream read
+        :return: the list of data bytes with at most max_read long
+        """
         data = []
         buffer = self.__recv_buffer
         self.__empty_cond.acquire()
@@ -61,4 +85,5 @@ class RecvBuffer:
             front = buffer[0].get_data()
         self.__resize_cond.notify()
         self.__empty_cond.release()
+        self.__logger.info("Data: " + data)
         return data
